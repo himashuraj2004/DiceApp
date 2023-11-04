27f311450857fca80f70a642622142a11aff94b6
himashuraj2004 <131295247+himashuraj2004@users.noreply.github.com>
2023-11-04 20:17:13 +0530

    DiceApp v0.2

--- app/src/main/java/com/example/happyrakshabandhan/MainActivity.kt
+++ app/src/main/java/com/example/happyrakshabandhan/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.happyrakshabandhan
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -16,46 +16,60 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener {
             dice1()
             dice2()
-            val textView1: TextView=findViewById(R.id.textView)
-            val textView2: TextView=findViewById(R.id.textView4)
-
-            val roll1=textView1.text.toString().toInt()
-            val roll2=textView2.text.toString().toInt()
-
-            if(roll1==roll2){
-                greet()
-            }
-            else {
-
-                remove()
-            }
         }
+        dice1()
+        dice2()
     }
-    private fun dice1() {
-        val Dice = dice(6)
-        val diceroll = Dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceroll.toString()
-    }
+
     private fun dice2() {
-        val Dice = dice(6)
-        val diceroll = Dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView4)
-        resultTextView.text = diceroll.toString()
-    }
-    private fun greet(){
-            val resultTextView: TextView=findViewById(R.id.textView2)
-            resultTextView.text="Hooraayy!!!"
+        val Dice=dice(6)
+        val roll=Dice.roll()
+
+        val diceImage: ImageView=findViewById(R.id.imageView2)
+        val drawableResource=when(roll){
+            1->R.drawable.dice_1
+            2->R.drawable.dice_2
+            3->R.drawable.dice_3
+            4->R.drawable.dice_4
+            5->R.drawable.dice_5
+            else->R.drawable.dice_6
         }
-    private fun remove(){
-        val removegreet: TextView=findViewById(R.id.textView2)
-         removegreet.text=""
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = roll.toString()
+
     }
-    }
-class dice(val sides: Int) {
-    fun roll(): Int {
-        return (1..sides).random()
+
+    /* Roll the dice and update the screen with the result.*/
+    private fun dice1() {
+        val Dice = dice(6) //creating a new dice with 6 sides
+        val diceroll = Dice.roll()
+
+        //find the imageView by giving the address
+        val diceImage: ImageView = findViewById(R.id.imageView)
+
+        //determining which dice face should be displayed with its resource ID
+        val drawableResource = when (diceroll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        //update the imgaeView with correct drawable object
+        diceImage.setImageResource(drawableResource)
+
+        //update the content description
+        diceImage.contentDescription = diceroll.toString()
     }
 
 }
+
+    class dice(val sides: Int) {
+        fun roll(): Int {
+            return (1..sides).random()
+        }
+
+    }
+
 
